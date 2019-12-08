4a21304c8ebac32b945075545fdb11d8ef8c2a4e
Pedro <pedro.n.martins@outlook.com>
2019-12-08 15:00:22 +0000

    Add project files.

---
+++ Database.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Box](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](50) NOT NULL,
+	[Description] [varchar](500) NOT NULL,
+	[Order] [int] NOT NULL,
+	[Active] [bit] Null,
+	PRIMARY KEY(Id)
+)
